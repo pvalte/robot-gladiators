4662ef4d38ece091cc3f7a17aa70450d0bfc9de4
Paige A <pvaltemare@gmail.com>
2021-08-26 21:50:58 -0700

    add fights

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,17 +1,23 @@
 var playerName = window.prompt("What is your robot's name?");
-console.log(playerName);
-console.log("This logs a string, good for leaving yourself a message");
-// this will do math and log 20
-console.log(10 + 10);
-// what is this?
-console.log("Our robot's name is " + playerName);
+var playerHealth = 100;
+var playerAttack = 10;
 
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
 
-// this creates a function named "fight"
-function fight() {
-    window.alert("The fight has begun for " + playerName);
-}
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
+var fight = function() {
+  window.alert("Welcome to Robot Gladiators!");
+  enemyHealth = enemyHealth - playerAttack;
+  console.log(playerName + " attacks " + enemyName);
+  console.log(enemyName + "'s health is " + enemyHealth);
 
+  console.log(enemyName + " attacks " + playerName);
+  playerHealth = playerHealth - enemyAttack;
+  console.log(playerName + "'s health is " + playerHealth);
+};
 
 fight();
