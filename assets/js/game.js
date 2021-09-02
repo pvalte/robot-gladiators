9cbd893f5be92eb54ccbe6c08376d1363dab684c
Paige A <pvaltemare@gmail.com>
2021-09-01 19:44:25 -0700

    Blank/null response handling for the player-name prompt

--- assets/js/game.js
+++ assets/js/game.js
@@ -172,9 +172,19 @@ var randomNumber = function(min, max) {
     return value;
 };
 
+var getPlayerName = function() {
+    var name = "";
+    while (name === "" || name === null) {
+        name = window.prompt("What is your robot's name?");
+    }
+    console.log("Your robot's name is " + name)
+    return name;
+;}
+
+
 // Player Robot
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
